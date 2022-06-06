5a61acc5f04ff0b50327ab958982c4f9ba68bba2
Guilherme <guilhermeevencioluz@gmail.com>
2022-06-06 10:32:37 -0300

    refactor: imc function operator.

--- introducao-node/index.js
+++ introducao-node/index.js
@@ -1,7 +1,8 @@
 const readline = require('readline-sync');
 const imc = require('./imc')
 
-const weight = readline.questionInt('Qual é o seu peso?');
-const height = readline.questionInt('Qual é a sua altura?');
+const weight = readline.questionFloat('Qual é o seu peso?');
+const height = readline.questionFloat('Qual é a sua altura?');
 
 const imcValue = imc(weight, height);
+console.log(imcValue)
