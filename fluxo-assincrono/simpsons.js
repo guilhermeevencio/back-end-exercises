b7640a6c6956cac1ce501f32c5f9bc620f4fb7f1
Guilherme <guilhermeevencioluz@gmail.com>
2022-06-08 09:45:32 -0300

    Crie uma função que substitua o personagem Nelson Muntz pela personagem Maggie Simpson no arquivo simpsonFamily.json.

--- fluxo-assincrono/simpsons.js
+++ fluxo-assincrono/simpsons.js
@@ -28,10 +28,8 @@ const removeChar = async () => {
     const response = await fs.readFile('simpsons.json', 'utf-8');
     const data = JSON.parse(response);
     const newArr = data.filter((simpson) => simpson.id !== '10' && simpson.id !== '6');
+
     await fs.writeFile('simpsons.json', JSON.stringify(newArr));
-
-    
-
   } catch (e) {
     console.log(e.message);
   }
@@ -42,11 +40,8 @@ const createNewFile = async () => {
     const response = await fs.readFile('simpsons.json', 'utf-8');
     const data = JSON.parse(response);
     const newArr = data.filter((simpson) => Number(simpson.id) < 5);
-    console.log(newArr);
+  
     await fs.writeFile('./simpsonFamily.json', JSON.stringify(newArr), { flag: 'wx' });
-
-    
-
   } catch (e) {
     console.log(e.message);
   }
@@ -60,18 +55,32 @@ const addNelson = async () => {
 
     const responseSimpson = await fs.readFile('simpsonFamily.json', 'utf-8');
     const dataSimpson = JSON.parse(responseSimpson);
+  
     const newArr = [...dataSimpson, nelson];
-    console.log(newArr);
     await fs.writeFile('./simpsonFamily.json', JSON.stringify(newArr));
-
-    
-
   } catch (e) {
     console.log(e.message);
   }
 }
 
-addNelson()
+const replaceNelson = async () => {
+  try {
+    const response = await fs.readFile('simpsons.json', 'utf-8');
+    const data = JSON.parse(response);
+    const maggie = data.find((simpson) => Number(simpson.id) === 5);
+  
+    const responseSimpson = await fs.readFile('simpsonFamily.json', 'utf-8');
+    const dataSimpson = JSON.parse(responseSimpson);
+    const removeNelson = dataSimpson.filter((simpson) => Number(simpson.id) !== 8);
+
+    const newArr = [...removeNelson, maggie];
+    await fs.writeFile('./simpsonFamily.json', JSON.stringify(newArr));
+  } catch (e) {
+    console.log(e.message);
+  }
+}
+
+replaceNelson();
 
 async function main() {
   await removeChar();
