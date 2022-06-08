8b25206e65d2566736f0830f64204b5cd06243bc
Guilherme <guilhermeevencioluz@gmail.com>
2022-06-08 15:21:26 -0300

    Adiciona todas as bibliotecas necessarias para a implementação dos testes e cria a estrutura de pastas do exercício.

---
+++ mocha-chai-sinon-testes/tests/isNumberPositive.js
@@ -0,0 +1,14 @@
+describe('Verifica o número', () => {
+  it('Retorna uma string como resposta', () => {
+
+  });
+  it('Quando o número passado for maior que 0, deverá retornar "positivo".', () => {
+
+  });
+  it('Quando o número passado for menor que 0, deverá retornar "negativo".', () => {
+
+  });
+  it('Quando o número passado for igual a 0, deverá retornar "neutro".', () => {
+
+  });
+});
