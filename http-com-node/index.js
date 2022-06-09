0fe508155e168be032f5b48f2eef6a6690d8f7b0
Guilherme <guilhermeevencioluz@gmail.com>
2022-06-09 19:20:13 -0300

    Crie uma rota POST /greetings.

--- http-com-node/index.js
+++ http-com-node/index.js
@@ -11,8 +11,18 @@ app.get('/ping', (req, res) => {
 
 app.post('/hello', (req, res) => {
   const { name } = req.body;
-  console.log(name);
   return res.status(200).json({'message': `Hello, ${name}!` });
+});
+
+app.post('/greetings', (req, res) => {
+  const { name, age } = req.body;
+
+  if( age > 17 ) {
+    return res.status(200).json({ "message": `Hello, ${ name }!` });
+  }
+
+  return res.status(401).json({ 'message': 'Unauthorized' });
+
 });
 
 app.listen(3333, () => {
