a29111db630b9c775c396c9c8946d678c96167b2
Guilherme <guilhermeevencioluz@gmail.com>
2022-06-09 18:55:39 -0300

    feat: Crie uma rota GET /ping

--- http-com-node/index.js
+++ http-com-node/index.js
@@ -2,3 +2,12 @@ const express = require('express');
 
 const app = express();
 
+app.use = express.json();
+
+app.get('/ping', (req, res) => {
+  return res.status(200).json({'message': 'pong' })
+});
+
+app.listen(3333, () => {
+  console.log('Server is running');
+})
