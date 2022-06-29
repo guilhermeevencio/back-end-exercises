49150acb71652f7d18cb4eed5270e9d306a463e3
Guilherme <guilhermeevencioluz@gmail.com>
2022-06-29 09:38:35 -0300

    feat: implementa os testes da camada de controllers.

--- testando-camadas-msc/movies-api/src/tests/services/movieService.test.js
+++ testando-camadas-msc/movies-api/src/tests/services/movieService.test.js
@@ -8,10 +8,10 @@ describe('Insere um nogvo filme bo BD', () => {
   describe('quando o payload informado não é válido', () => {
     it('retorn um boolean', async () => {
       const response = await MoviesService.create(payloadMovie);
-      expect (response).to.be.a('boolean');
+      expect(response).to.be.a('boolean');
     });
 
-    it('O boolean contém false', async() => {
+    it('O boolean contém false', async () => {
       const response = await MoviesService.create(payloadMovie);
       expect(response).to.be.equal(false);
     })
@@ -23,19 +23,19 @@ describe('Insere um nogvo filme bo BD', () => {
       directedBy: 'Jane Dow',
       releaseYear: 1999,
     };
-  
-    before (()=> {
+
+    before(() => {
       const ID_EXAMPLE = 1;
 
-      sinon.stub(MoviesModel, 'create').resolves( {id: ID_EXAMPLE});
+      sinon.stub(MoviesModel, 'create').resolves({ id: ID_EXAMPLE });
     });
 
     after(() => MoviesModel.create.restore());
-  
+
     it('retorna um objeto', async () => {
       const response = await MoviesService.create(payloadMovie);
 
-      expect (response).to.have.a.property('id');
+      expect(response).to.have.a.property('id');
     });
 
     it('tal objeto possui o "id" do novo filme inserido', async () => {
